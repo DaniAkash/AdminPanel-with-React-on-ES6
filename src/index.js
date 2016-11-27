7367818b271ddc0a86a939d1e90ca38f6ec55222
DaniAkash <eaglearrow.arrow@gmail.com>
2016-11-27 15:42:54 +0530

    courses page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import {render} from 'react-dom';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import {Router, browserHistory} from 'react-router';
+import {loadCourses} from './actions/courseActions';
 import routes from './routes';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render (
   <Provider store={store}>
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -3,10 +3,13 @@ import * as types from '../actionTypes/actionTypes';
 export default function courseReducer(state = [], action) {
   switch (action.type) {
 
-    case types.CREATE_COURSE:
-      return [...state,
-        Object.assign({}, action.course)
-      ];
+    // case types.CREATE_COURSE:
+    //   return [...state,
+    //     Object.assign({}, action.course)
+    //   ];
+
+    case types.LOAD_COURSES_SUCCESS:
+      return action.courses;
 
     default:
       return state;
